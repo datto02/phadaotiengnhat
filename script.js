46ae1ca86eb843a99203f92dcadd748f704cfe0a
datto02 <vuthedat333@gmail.com>
2026-01-22 18:56:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -400,7 +400,6 @@ const ReviewListModal = ({ isOpen, onClose, srsData, onResetSRS }) => {
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
                                     XÓA TOÀN BỘ TIẾN ĐỘ
                                 </button>
-                                <p className="text-[9px] text-gray-400 mt-1">Hành động này sẽ xóa mọi dữ liệu về SRS</p>
                             </div>
                         </div>
                     </>
